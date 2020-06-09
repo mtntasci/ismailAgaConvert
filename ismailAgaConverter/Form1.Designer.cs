13934da99fa804015048569ef3e62776174da89d
mt <mtntasci@gmail.com>
2020-06-09 13:10:53 +0300

    Add project files.

---
+++ ismailAgaConverter/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ismailAgaConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOzluSozler = new System.Windows.Forms.Button();
+            this.btnTarihteBugün = new System.Windows.Forms.Button();
+            this.txtjson = new System.Windows.Forms.TextBox();
+            this.btnArkaSayfa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOzluSozler
+            // 
+            this.btnOzluSozler.Location = new System.Drawing.Point(12, 22);
+            this.btnOzluSozler.Name = "btnOzluSozler";
+            this.btnOzluSozler.Size = new System.Drawing.Size(128, 23);
+            this.btnOzluSozler.TabIndex = 0;
+            this.btnOzluSozler.Text = "Özlü Sözler";
+            this.btnOzluSozler.UseVisualStyleBackColor = true;
+            this.btnOzluSozler.Click += new System.EventHandler(this.btnOzluSozler_Click);
+            // 
+            // btnTarihteBugün
+            // 
+            this.btnTarihteBugün.Location = new System.Drawing.Point(12, 61);
+            this.btnTarihteBugün.Name = "btnTarihteBugün";
+            this.btnTarihteBugün.Size = new System.Drawing.Size(128, 23);
+            this.btnTarihteBugün.TabIndex = 1;
+            this.btnTarihteBugün.Text = "Tarihte Bugün";
+            this.btnTarihteBugün.UseVisualStyleBackColor = true;
+            this.btnTarihteBugün.Click += new System.EventHandler(this.btnTarihteBugün_Click);
+            // 
+            // txtjson
+            // 
+            this.txtjson.Dock = System.Windows.Forms.DockStyle.Right;
+            this.txtjson.Location = new System.Drawing.Point(164, 0);
+            this.txtjson.Multiline = true;
+            this.txtjson.Name = "txtjson";
+            this.txtjson.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtjson.Size = new System.Drawing.Size(636, 450);
+            this.txtjson.TabIndex = 2;
+            // 
+            // btnArkaSayfa
+            // 
+            this.btnArkaSayfa.Location = new System.Drawing.Point(12, 103);
+            this.btnArkaSayfa.Name = "btnArkaSayfa";
+            this.btnArkaSayfa.Size = new System.Drawing.Size(128, 23);
+            this.btnArkaSayfa.TabIndex = 3;
+            this.btnArkaSayfa.Text = "Arka Sayfa";
+            this.btnArkaSayfa.UseVisualStyleBackColor = true;
+            this.btnArkaSayfa.Click += new System.EventHandler(this.btnArkaSayfa_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnArkaSayfa);
+            this.Controls.Add(this.txtjson);
+            this.Controls.Add(this.btnTarihteBugün);
+            this.Controls.Add(this.btnOzluSozler);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "İsmailağa Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOzluSozler;
+        private System.Windows.Forms.Button btnTarihteBugün;
+        public System.Windows.Forms.TextBox txtjson;
+        private System.Windows.Forms.Button btnArkaSayfa;
+    }
+}
+
---
+++ ismailAgaConverter/Form1.cs
@@ -0,0 +1,301 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+
+namespace ismailAgaConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOzluSozler_Click(object sender, EventArgs e)
+        {
+            txtjson.Text = "";
+            List<ozluSoz> _ozluSozler = OzluSozler.GetJson();
+            string json = JsonConvert.SerializeObject(_ozluSozler, Formatting.Indented);
+            txtjson.Text = json;
+        }
+        sealed class OzluSozler
+        {
+
+            private static List<ozluSoz> _instance =
+              new List<ozluSoz>();
+
+            private List<ozluSoz> _ozluSoz;
+            private OzluSozler()
+            {               
+            }
+
+            public static List<ozluSoz> GetJson()
+            {
+                string path = "C:\\Docs\\2020-ozlu-sozler.docx";
+                Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application();
+                object file = path;
+                object nullobj = System.Reflection.Missing.Value;
+                Microsoft.Office.Interop.Word.Document doc = wordApp.Documents.Open(ref file, ref nullobj, ref nullobj,
+                ref nullobj, ref nullobj, ref nullobj,
+                ref nullobj, ref nullobj, ref nullobj,
+                ref nullobj, ref nullobj, ref nullobj);
+                doc.ActiveWindow.Selection.WholeStory();
+                doc.ActiveWindow.Selection.Copy();
+                doc.Close();
+                IDataObject data = Clipboard.GetDataObject();
+                string txt = data.GetData(DataFormats.Text).ToString();
+                string[] _lines = Regex.Split(txt, Environment.NewLine);
+
+                int j = 0;
+
+                _instance = new List<ozluSoz>();
+                ozluSoz oz = new ozluSoz();
+
+                DateTime startDate = Convert.ToDateTime("31.12.2019");
+
+                for (int i = 0; i < _lines.Length; i++)
+                {
+
+                    if (_lines[i] != "Ocak" && _lines[i] != "Şubat" && _lines[i] != "Mart" && _lines[i] != "Nisan"
+                        && _lines[i] != "Mayıs" && _lines[i] != "Haziran" && _lines[i] != "1 Temmuz" && _lines[i] != "Ağustos"
+                        && _lines[i] != "Eylül" && _lines[i] != "Ekim" && _lines[i] != "Kasım" && _lines[i] != "Aralık")
+                    {
+
+                        j = j + 1;
+                        oz = new ozluSoz();
+                        oz.word_id = j.ToString();
+                        string monthName = CultureInfo.CurrentCulture.DateTimeFormat.GetAbbreviatedMonthName(startDate.AddDays(j).Month);
+                        oz.TarihSayi = (startDate.AddDays(j).Day.ToString() + "." + startDate.AddDays(j).Month.ToString() + "." + startDate.AddDays(j).Year.ToString());
+                        oz.TarihYazi = (startDate.AddDays(j).Day.ToString() + " " + monthName + " " + startDate.AddDays(j).Year.ToString());
+                        oz.Onsoz = _lines[i].ToString();
+                        _instance.Add(oz);
+
+                    }
+
+                }
+                return _instance;
+            }
+
+          
+        }
+
+        class ozluSoz
+        {
+            public string word_id { get; set; }
+            public string TarihYazi { get; set; }
+            public string TarihSayi { get; set; }
+            public string Onsoz { get; set; }
+
+        }
+
+        private void btnTarihteBugün_Click(object sender, EventArgs e)
+        {
+            txtjson.Text = "";
+            txtjson.Text = "";
+            List<tarihteBugun> _tarihteBugun = TarihteBugun.GetJson();
+            string json = JsonConvert.SerializeObject(_tarihteBugun, Formatting.Indented);
+            txtjson.Text = json;
+        }
+
+        sealed class TarihteBugun
+        {
+
+            private static List<tarihteBugun> _tarihteBugunInstance =
+              new List<tarihteBugun>();
+
+            private List<tarihteBugun> _tarihteBugun;
+            private TarihteBugun()
+            {
+            }
+
+            public static List<tarihteBugun> GetJson()
+            {
+                string path = "C:\\Docs\\2020-tarihte-bugun.docx";
+                Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application();
+                object file = path;
+                object nullobj = System.Reflection.Missing.Value;
+                Microsoft.Office.Interop.Word.Document doc = wordApp.Documents.Open(ref file, ref nullobj, ref nullobj,
+                ref nullobj, ref nullobj, ref nullobj,
+                ref nullobj, ref nullobj, ref nullobj,
+                ref nullobj, ref nullobj, ref nullobj);
+                doc.ActiveWindow.Selection.WholeStory();
+                doc.ActiveWindow.Selection.Copy();
+                doc.Close();
+                IDataObject data = Clipboard.GetDataObject();
+                string txt = data.GetData(DataFormats.Text).ToString();
+                string[] _lines = Regex.Split(txt, Environment.NewLine);
+
+                int j = 0;
+
+                _tarihteBugunInstance = new List<tarihteBugun>();
+                tarihteBugun oz = new tarihteBugun();
+
+                DateTime startDate = Convert.ToDateTime("01.01.2020");
+                string notes = string.Empty;
+
+                for (int i = 0; i < _lines.Length; i++)
+                {
+                    bool result = true;
+                    DateTime dt = Convert.ToDateTime("31.12.2019"); 
+
+                    try
+                    {
+                       dt = Convert.ToDateTime(_lines[i] + " 2020");
+                    }
+                    catch (Exception)
+                    {
+                        result = false;
+
+                    }
+                    if (result == true)
+                    {
+                        
+                        if (notes != string.Empty)
+                        {
+                            startDate = dt.AddDays(-1);
+                            j = j + 1;
+                            oz = new tarihteBugun();
+                            oz.history_id = j.ToString();
+                            string monthName = CultureInfo.CurrentCulture.DateTimeFormat.GetAbbreviatedMonthName(startDate.Month);
+                            oz.TarihSayi = (startDate.Day.ToString() + "." + startDate.Month.ToString() + "." + startDate.AddDays(j).Year.ToString());
+                            oz.TarihYazi = (startDate.Day.ToString() + " " + monthName + " " + startDate.Year.ToString());
+                            oz.GununOnemi = notes;
+                            _tarihteBugunInstance.Add(oz);
+                            notes = string.Empty;
+                        }
+                    }
+                    if (result== false)
+                    {
+                        notes = notes + "- " + _lines[i].ToString() + " ";
+                        
+
+                    }
+
+                }
+                return _tarihteBugunInstance;
+            }
+            public bool isDate(string lineText)
+            {
+                bool result = false;
+
+                try
+                {
+                    DateTime dt = Convert.ToDateTime(lineText + " 2020");
+                }
+                catch (Exception)
+                {
+
+
+                }
+                finally
+                {
+                    result = true;
+                }
+
+                return result;
+            }
+
+        }
+
+        class tarihteBugun
+        {
+            public string history_id { get; set; }
+            public string TarihYazi { get; set; }
+            public string TarihSayi { get; set; }
+            public string GununOnemi { get; set; }
+
+        }
+
+      
+
+        private void btnArkaSayfa_Click(object sender, EventArgs e)
+        {
+            //txtjson.Text = "";
+            //List<arkaSayfa> _arkaSayfa = ArkaSayfa.GetJson();
+            //string json = JsonConvert.SerializeObject(_arkaSayfa, Formatting.Indented);
+            //txtjson.Text = json;
+        }
+
+        sealed class ArkaSayfa
+        {
+
+            private static List<arkaSayfa> _arkaSayfaInstance =
+              new List<arkaSayfa>();
+
+            private List<arkaSayfa> _arkaSayfa;
+            private ArkaSayfa()
+            {
+            }
+
+            public static List<arkaSayfa> GetJson()
+            {
+                string path = "C:\\Docs\\2020-tarihte-bugun.docx";
+                Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application();
+                object file = path;
+                object nullobj = System.Reflection.Missing.Value;
+                Microsoft.Office.Interop.Word.Document doc = wordApp.Documents.Open(ref file, ref nullobj, ref nullobj,
+                ref nullobj, ref nullobj, ref nullobj,
+                ref nullobj, ref nullobj, ref nullobj,
+                ref nullobj, ref nullobj, ref nullobj);
+                doc.ActiveWindow.Selection.WholeStory();
+                doc.ActiveWindow.Selection.Copy();
+                doc.Close();
+                IDataObject data = Clipboard.GetDataObject();
+                string txt = data.GetData(DataFormats.Text).ToString();
+                string[] _lines = Regex.Split(txt, Environment.NewLine);
+
+                int j = 0;
+
+                _arkaSayfaInstance = new List<arkaSayfa>();
+                arkaSayfa oz = new arkaSayfa();
+
+                DateTime startDate = Convert.ToDateTime("31.12.2019");
+
+                for (int i = 0; i < _lines.Length; i++)
+                {
+
+                    if (_lines[i] != "Ocak" && _lines[i] != "Şubat" && _lines[i] != "Mart" && _lines[i] != "Nisan"
+                        && _lines[i] != "Mayıs" && _lines[i] != "Haziran" && _lines[i] != "Temmuz" && _lines[i] != "Ağustos"
+                        && _lines[i] != "Eylül" && _lines[i] != "Ekim" && _lines[i] != "Kasım" && _lines[i] != "Aralık")
+                    {
+
+                        j = j + 1;
+                        oz = new arkaSayfa();
+                        oz.takvim_id = j.ToString();
+                        string monthName = CultureInfo.CurrentCulture.DateTimeFormat.GetAbbreviatedMonthName(startDate.AddDays(j).Month);
+                        oz.takvim_tarih = (startDate.AddDays(j).Day.ToString() + "." + startDate.AddDays(j).Month.ToString() + "." + startDate.AddDays(j).Year.ToString());
+                        oz.takvim_baslik = (startDate.AddDays(j).Day.ToString() + " " + monthName + " " + startDate.AddDays(j).Year.ToString());
+                        oz.takvim_aciklama = _lines[i].ToString();
+                        _arkaSayfaInstance.Add(oz);
+
+                    }
+
+                }
+                return _arkaSayfaInstance;
+            }
+
+
+        }
+
+        class arkaSayfa
+        {
+            public string takvim_id { get; set; }
+            public string takvim_baslik { get; set; }
+            public string takvim_tarih { get; set; }
+            public string takvim_aciklama { get; set; }
+
+        }
+    }
+}
+
